6da30df91f8c10ad9e03eb25a30a0579a5eee977
kevton <kevin.clerima@outlook.fr>
2026-04-02 18:20:08 +0200

    Implémentation de l'inscription utilisateur avec persistance en base de données

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,14 +14,27 @@ use Symfony\Component\Routing\Attribute\Route;
 final class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_inscription')]
-    public function index(Request $request): Response
+    public function index(EntityManagerInterface $em, Request $request): Response
     {
         $user = new User();
         $form =$this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid()){
+            try {
+                $user->setCreateAt(new \DateTimeImmutable());
+                $user->setRoles(['ROLE_USER']);
+                $em->persist($user);
+                $em->flush();
+                return $this->redirectToRoute('app_home');
+            } catch (EntityNotFoundException $e) {
+                echo $e;
+            }
+
+        }
 
         return $this->render('register/index.html.twig', [
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 }
